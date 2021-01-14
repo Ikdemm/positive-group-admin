b5c6a5fa7b7affde5eb748f41a9661d2461e9180
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-01-14 22:44:29 +0000

    create course component

--- client/src/app/views/courses/courses.module.ts
+++ client/src/app/views/courses/courses.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CourseComponent } from './courses-list/course/course.component';
 
 @NgModule({
-  declarations: [AddCourseComponent, CoursesListComponent],
+  declarations: [AddCourseComponent, CoursesListComponent, CourseComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule
