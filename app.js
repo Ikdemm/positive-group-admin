6107f4fceb0a66a6fc0e3481b08fbb257486a095
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-01-16 15:20:51 +0000

    client side rendering

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 app.use('/', express.static(path.join(__dirname, 'client/dist')))
 
 // Send back the 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
 
