9a1496f4e29357e4547caa02916e358d4c60f85f
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-01-16 23:58:16 +0000

    added a get request to fetch courses

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Category = require('../models/category');
 
+// Retrieving existing Categories
+router.get('/', (req, res) => {
+    console.log("hey")
+    Category.find({}, (err, categories) => {
+        if (err) return handleError(err);
+        console.log(categories);
+        res.send(categories)
+    })
+})
+
 // Creating new categories
 router.post('/', (req, res) => {
     const categoryData = {
@@ -13,16 +23,6 @@ router.post('/', (req, res) => {
     newCategory.save((err, newCategory) => {
         if (err) return handleError(err);
         res.send(newCategory)
-    })
-})
-
-// Retrieving existing Categories
-router.get('/', (req, res) => {
-    console.log("hey")
-    Category.find({}, (err, categories) => {
-        if (err) return handleError(err);
-        console.log(categories);
-        res.send(categories)
     })
 })
 
--- src/routes/courses.route.js
+++ src/routes/courses.route.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Course = require('../models/course');
 
+// Retrieving existing Categories
+router.get('/', (req, res) => {
+    Course.find({}, (err, courses) => {
+        if (err) return handleError(err);
+        console.log(courses);
+        res.send(courses)
+    })
+})
+
+// Creating new courses
 router.post('/', (req, res) => {
     const courseData = {
         name: req.body.name,
