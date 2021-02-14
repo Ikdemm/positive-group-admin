73bf7f3552e132e72b04f3e3ff69be0a09a850c9
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-14 23:18:24 +0000

    used repository in courses controller

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -12,85 +12,33 @@ module.exports = {
         }
     },
 
-    createChapter: async (req, res) => {
+    createCourse: async (req, res) => {
         try {
-            const chapter = await repository.save(req.body, Chapter)
-            res.status(200).send(chapter)
+            const course = await repository.save(req.body, Course)
+            res.status(200).send(course)
         }
         catch (e) {
             console.error(e);
         }
     },
 
-    deleteChapter: async (req, res) => {
+    deleteCourse: async (req, res) => {
         try {
-            const deletedChapter = await repository.delete(req.params.id, Chapter)
-            res.status(200).send(deletedChapter)
+            const deletedCourse = await repository.delete(req.params.id, Course)
+            res.status(200).send(deletedCourse)
         }
         catch (e) {
             console.error(e);
         }
     },
 
-    updateChapter: async (req, res) => {
+    updateCourse: async (req, res) => {
         try {
-            const updatedChapter = await repository.updateOne(req.params.id, req.body, Chapter)
-            res.status(200).send(updatedChapter)
+            const updatedCourse = await repository.updateOne(req.params.id, req.body, Course)
+            res.status(200).send(updatedCourse)
         }
         catch (e) {
             console.error(e);
         }
-    }
-    
-    getAllCourses: (req, res) => {
-        Course.find({}, (err, courses) => {
-            if (err) return handleError(err);
-            res.send(courses);
-        });
-    },
-
-    createCourse: (req, res) => {
-        let courseData = req.body;
-        courseData.date = new Date();
-        let newCourse = new Course(courseData);
-
-        newCourse.save((newCourse))
-            .then(() => {
-                res.send(newCourse);
-            })
-            .catch((error) => {
-                res.status(400).json({
-                    error: error,
-                });
-            });
-    },
-
-    deleteCourse: (req, res) => {
-        Course.deleteOne({ _id: req.params.id })
-            .then(() => {
-                res.status(200).json({
-                    message: "Deleted!",
-                });
-            })
-            .catch((error) => {
-                res.status(400).json({
-                    error: error,
-                });
-            });
-    },
-
-    updateCourse: (req, res) => {
-        let updatedCourse = req.body;
-        Course.updateOne({ _id: req.params.id }, updatedCourse)
-            .then(() => {
-                res.status(200).json({
-                    message: "Updated!",
-                });
-            })
-            .catch((error) => {
-                res.status(400).json({
-                    error: error,
-                });
-            });
     }
 }
