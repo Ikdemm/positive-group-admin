ecb49e505a115a07841f3b5689260628699c7e73
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-16 00:26:22 +0000

    added signup to auth controller and refactored login to async await

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+
+const bcrypt = require("bcrypt")
 
 // Using Body-Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,30 +1,39 @@
 const Admin = require("../models/admin");
+const User = require("../models/user");
+const repository = require("../repositories/repository");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-    login: (req, res) => {
-        Admin.findOne({ email: req.body.email })
-            .then((admin) => {
-                if (admin) {
-                    if (admin.password === req.body.password) {
-                        res.send({
-                            message: "SUCESS!",
-                        });
-                    } else {
-                        res.send({
-                            message: "WRONG PASSWORD",
-                        });
-                    }
-                } else {
-                    res.send({
-                        message: "ADMIN NOT FOUND!",
-                    });
-                }
-            })
-            .catch((error) => {
-                console.log("error: ", error);
-                res.status(400).json({
-                    error: error,
+    login: async (req, res) => {
+        try {
+            const admin = await Admin.findOne({ email: req.body.email });
+            if (admin.password === req.body.password) {
+                res.send({
+                    message: "SUCESS!",
                 });
+            } else {
+                res.send({
+                    message: "WRONG PASSWORD",
+                });
+            }
+        }
+        catch (e) {
+            res.status(400).json({
+                error: error,
             });
+        }
+    },
+
+    signup: async (req, res) => {
+        try {
+            let userData = req.body;
+            const salt = await bcrypt.genSalt() // We'll just leave the default value 10
+            userData.password = await bcrypt.hash(userData.password, salt);
+            const user = repository.save(user, User);
+            res.status(201).send({ message: "Created!" })
+        }
+        catch (e) {
+            console.error(e)
+        }
     }
 }
