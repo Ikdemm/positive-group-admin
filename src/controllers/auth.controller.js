efbd374bd957abd97a45f6939b7926eff16bb333
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-18 23:40:26 +0000

    refactored admin reset password to use auth repository

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,6 @@
 require("dotenv").config()
 
 const Admin = require("../models/admin");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
-const repository = require("../repositories/base.repository");
-const emailRepository = require('../repositories/email.repository');
 const authRepository = require('../repositories/auth.repository')
 
 module.exports = {
@@ -39,50 +34,11 @@ module.exports = {
 
     forgotPassword: async (req, res) => {
         try {
-            /**
-             * Fetch admin from Database
-             */
-
-            const { email } = req.body;
-            const admin = await repository.findOne({ email: email }, Admin);
-
-            /**
-             * Check if an admin exists with that email
-             */
-
-            if (!admin) {
-                res.status(500).json({ message: "No such email" })
-            }
-
-            /**
-             * Create a one time link valid for 15 minutes
-             */
-
-            const secret = process.env.ACCESS_TOKEN_SECRET + email;
-            const payload = {
-                email: admin.email,
-                id: admin._id
-            }
-            const token = jwt.sign(payload, secret, { expiresIn: '15m' })
-            const link = `http://localhost:8000/reset-password/${admin._id}/${token}`
-
-            /**
-             * Send the link to the user via email
-             */
-
-            emailRepository.sendEmail(admin.email, "Password Reset", link)
-                .then((error, info) => {
-                    if (error) {
-                        res.send(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                });
-
-            res.status(200).send("check your email")
-
-        } catch (error) {
-            res.send(error)
+            const { status, message } = await authRepository.resetPassword(req.body, Admin)
+            res.status(status).json(message)
+        }
+        catch (e) {
+            res.status(500).send({ error: e })
         }
 
     }
--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -3,7 +3,8 @@ require("dotenv")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const baseRepository = require("./base.repository")
+const baseRepository = require("./base.repository");
+const emailRepository = require("./email.repository")
 
 module.exports = {
 
@@ -49,11 +50,60 @@ module.exports = {
             /** 
              *  Save the new admin to the database
              */
-            const admin = await repository.save(adminData, model);
+            const admin = await baseRepository.save(adminData, model);
             return ({ status: 201, message: "Created!" })
         }
         catch (e) {
             throw (e)
         }
     },
+
+    resetPassword: async (data, model) => {
+        try {
+            /**
+             * Fetch admin from Database
+             */
+
+            const { email } = data;
+            const admin = await baseRepository.findOne({ email: email }, model);
+
+            /**
+             * Check if an admin exists with that email
+             */
+
+            if (!admin) {
+                return ({ status: 500, message: "No such email" })
+            }
+
+            /**
+             * Create a one time link valid for 15 minutes
+             */
+
+            const secret = process.env.ACCESS_TOKEN_SECRET + email;
+            const payload = {
+                email: admin.email,
+                id: admin._id
+            }
+            const token = jwt.sign(payload, secret, { expiresIn: '15m' })
+            const link = `http://localhost:8000/reset-password/${admin._id}/${token}`
+
+            /**
+             * Send the link to the user via email
+             */
+
+            emailRepository.sendEmail(admin.email, "Password Reset", link)
+                .then((error, info) => {
+                    if (error) {
+                        throw error
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+
+            return ({ status: 200, message: "check your email" })
+
+        } catch (error) {
+            throw error
+        }
+    }
 }
