55b68e73a9aa2e4327fe79a86469ad23266c6c47
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-02-21 00:00:17 +0000

    refactored multer to be in a seperate middleware

---
+++ src/middlewares/multer.js
@@ -0,0 +1,34 @@
+const multer = require('multer');
+const { callbackPromise } = require("nodemailer/lib/shared");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/uploads');
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, req.body.name + "." + ext)
+    }
+})
+
+/* 
+    File filter to only accept images
+**/
+
+const fileFilter = (req, file, cb) => {
+    // accept image files
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        // Reject other types of files
+        cb(null, false)
+    }
+}
+
+module.exports = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    }
+});
--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -2,40 +2,7 @@ const router = require("express").Router();
 const categoriesController = require("../controllers/categories.controller");
 const authenticateToken = require("../middlewares/authenticateToken");
 
-const multer = require('multer');
-const { callbackPromise } = require("nodemailer/lib/shared");
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'src/uploads');
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split("/")[1];
-        cb(null, req.body.name + "." + ext)
-    }
-})
-
-/* 
-    File filter to only accept images
-**/
-
-const fileFilter = (req, file, cb) => {
-    // accept image files
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        // Reject other types of files
-        cb(null, false)
-    }
-}
-
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    }
-});
+const upload = require("../middlewares/multer")
 
 // Retrieving existing Categories
 
