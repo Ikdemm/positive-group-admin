414bb7680fe4a98d407350ccf42ac5734b79251a
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-05-27 12:30:15 +0100

    bonus init

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const repository = require("../repositories/base.repository");
-const { catchAsync } = require("../helpers")
+const bonusService = require("../services/bonus.service");
+const { catchAsync } = require("../helpers");
 
 module.exports = {
 
@@ -22,62 +23,8 @@ module.exports = {
     getInvitees: catchAsync(async (req, res) => {
         // Get user id from params
         let userId = req.params.userId;
-
-        // Get level 1 invitees
-        let user = await User.findById(userId);
-        let level1Invitees = user.invitees;
-
-        let response = {}
-
-        for (let i = 1; i <= 10; i++) {
-            response[`level${i}Invitees`] = []
-        }
-
-        await Promise.all(level1Invitees.map(async (invitee) => {
-            let currentInvitee = await User.findById(invitee);
-            if (currentInvitee != null) {
-                let responseInvitee = {
-                    inviteeId: invitee,
-                    accountType: currentInvitee.accountType,
-                    courses: currentInvitee.courses.length
-                }
-                response.level1Invitees.push(responseInvitee)
-            } else {
-                User.findByIdAndUpdate(invitee, { $pullAll: { invitees: [invitee] } })
-            }
-
-        }))
-
-        for (let i = 1; i < 10; i++) {
-
-            try {
-                await Promise.all(
-                    response[`level${i}Invitees`].map(async (invitee) => {
-                        let invitedUsers = await User.findById(invitee.inviteeId).select({ "invitees": 1, "_id": 0 })
-                        if (invitedUsers.invitees) {
-                            await Promise.all(invitedUsers.invitees.map(async (inviteeId) => {
-                                let currentInvitee = await User.findById(inviteeId);
-                                if (currentInvitee != null) {
-                                    let responseInvitee = {
-                                        inviteeId: inviteeId,
-                                        accountType: currentInvitee.accountType,
-                                        courses: currentInvitee.courses.length
-                                    }
-                                    response[`level${i + 1}Invitees`].push(responseInvitee)
-                                } else {
-                                    User.findByIdAndUpdate(inviteeId, { $pullAll: { invitees: [inviteeId] } })
-                                }
-                            }))
-                        }
-                    })
-                )
-            } catch (err) {
-                console.error(err)
-            }
-        }
-
-        res.status(200).send(response)
-
+        let bonusTree = bonusService.getBonusTree(userId)
+        res.status(200).send(bonusTree)
     }),
 
     assignDefaultInviter: catchAsync(async (req, res) => {
